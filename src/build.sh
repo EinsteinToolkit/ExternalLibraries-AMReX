a4a19f8f175201fdc9484f4b8f39dd4919017d0a
Roland Haas <rhaas@illinois.edu>
2021-08-06 18:00:07 -0500

    AMReX: update tarball to 20.08

--- src/build.sh
+++ src/build.sh
@@ -14,7 +14,7 @@ set -e                          # Abort on errors
 
 # Set locations
 THORN=AMReX
-NAME=amrex-36a98cdd9
+NAME=amrex-20.08
 SRCDIR="$(dirname $0)"
 BUILD_DIR=${SCRATCH_BUILD}/build/${THORN}
 if [ -z "${AMREX_INSTALL_DIR}" ]; then
